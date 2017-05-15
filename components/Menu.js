219d5a352a16c54c10fb4342a9d1432ec80526af
Hardys <hardyscc@gmail.com>
2017-05-15 17:29:07 +0800

    .

--- components/Menu.js
+++ components/Menu.js
@@ -1,9 +1,10 @@
 import { Menu } from 'semantic-ui-react'
+import Router from 'next/router'
 
 export default ({ pathname }) => (
   <Menu inverted pointing>
-    <Menu.Item name='index' active={pathname === '/'} href='/' />
-    <Menu.Item name='about' active={pathname === '/about'} href='/about' />
+    <Menu.Item name='index' active={pathname === '/'} onClick={() => Router.push('/')} />
+    <Menu.Item name='about' active={pathname === '/about'} onClick={() => Router.push('/about')} />
     <Menu.Menu position='right'>
        <Menu.Item name='logout' onClick={() => alert('window close')} />
     </Menu.Menu>
